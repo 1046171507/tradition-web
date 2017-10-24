3f14e3b208475005b9fa8706156c34472dc5a1d7
USER-20170721YY\Administrator <1046171507@qq.com>
2017-10-24 18:02:28 +0800

    vue客户端加载组件

---
+++ _vue-客户端组件式开发/components/a.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="a">
+		<span>我是A组件的数据->{{a}}</span>
+		<input type="button" value="把A数据传给父组件->进一步传递给B" @click="send">
+		<dom-c></dom-c>
+	</div>
+</template>
+
+<script>
+	var domC = requireVue('components/c.vue');
+	var dataA = "我是a组件中数据";
+	export default {
+		name: 'A',
+		components: {
+			"dom-c": domC
+		},
+		methods: {
+			send: function() {
+				this.$parent.childrenA = this.a
+			}
+		},
+		data: function() {
+			return {
+				a: dataA
+			}
+		},
+		mounted: function() { //类似于回调函数(初次实例化完成后调用)
+			console.log(this)
+		},
+	}
+</script>
+
+<style>
+	.a {
+		background-color: #CCCCCC;
+	}
+	/*能同步加入能同步删除,scoped不生效*/
+</style>
