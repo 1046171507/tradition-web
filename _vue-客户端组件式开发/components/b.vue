bca66288ca50521d5af8bc1c29f906b059507b6c
USER-20170721YY\Administrator <1046171507@qq.com>
2017-10-24 18:01:31 +0800

    vue客户端加载

---
+++ _vue-客户端组件式开发/components/b.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="b">
+		<h3>我是B组件-->接收父组件的数据为: {{childrenA}}</h3>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'B',
+		data: function() {
+			return {
+				a: ""
+			}
+		},
+		props: ['children-a'],
+		//钩子,类似于回调函数(实例化完成后调用)
+		mounted: function() {},
+	}
+</script>
+
+<style scoped>
+	.b {
+		background-color: #EEEEEE;
+	}
+	/*能同步加入能同步删除,scoped不生效*/
+</style>
---
+++ _vue-客户端组件式开发/components/c.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h4>我是A的子组件</h4>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'C',
+	}
+</script>
+
+<style scoped>
+
+</style>
