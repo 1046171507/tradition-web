053b2ad298344af1d82e7392ac46e63d2ad6e8cb
USER-20170721YY\Administrator <1046171507@qq.com>
2017-10-30 13:41:44 +0800

    no message

--- js/app/nav.js
+++ js/app/nav.js
@@ -1,6 +1,6 @@
 $(function() {
 	console.log('nav.html加载了');
-	$('#a-1 span').trigger('click'); //触发a标签的系统事件,要用触发a子级的事件冒泡实现
+	//$('#a-1 span').trigger('click'); //触发a标签的系统事件,要用触发a子级的事件冒泡实现
 	$('a').click(function () {
 		console.log('a标签点击')
 	});
